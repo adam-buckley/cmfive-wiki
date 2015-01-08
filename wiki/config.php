c07df78c4eaa0a89be9d1acedf6bd71cdd794092
careck <careck@gmail.com>
2015-01-09 10:39:53 +1100

    updated from cmfive master

--- wiki/config.php
+++ wiki/config.php
@@ -1,8 +1,9 @@
 <?php
 
-$modules['wiki'] = array(
+Config::set('wiki', array(
+    'version' => '0.8.0',		
     'active' => true,
     'path' => 'system/modules',
     'topmenu' => true,
     'search' => array("Wiki Pages" => "WikiPage")
-);
+));
