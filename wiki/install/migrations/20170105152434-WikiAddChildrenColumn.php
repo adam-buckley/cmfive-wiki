ef5537211c2383965a5f1491dc7ce8b1cf82f311
Carsten Eckelmann <careck@gmail.com>
2017-01-06 12:41:28 +1100

    fixed migration to add children col to page history

--- wiki/install/migrations/20170105152434-WikiAddChildrenColumn.php
+++ wiki/install/migrations/20170105152434-WikiAddChildrenColumn.php
@@ -10,10 +10,21 @@ class WikiAddChildrenColumn extends CmfiveMigration {
 		if (empty($column)) {
 			$table->addTextColumn('children',true,null)->update();
 		}
+		$table = $this->table('wiki_page_history');
+		if (!empty($table)) {
+			$column = $table->hasColumn('children');
+		}
+		if (empty($column)) {
+			$table->addTextColumn('children',true,null)->update();
+		}
 	}
 
 	public function down() {
 		$table = $this->table('wiki_page');
+		if (!empty($table)) {
+			$table->removeColumn('children')->update();
+		}
+		$table = $this->table('wiki_page_history');
 		if (!empty($table)) {
 			$table->removeColumn('children')->update();
 		}
