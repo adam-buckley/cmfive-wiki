037d8c2aed54f4fdc2e00f7cd8d21cb8fc9ca545
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2019-09-12 11:25:17 +1000

    Changed text limit to use TEXT_LONG static variable

--- wiki/install/migrations/20190911131531-WikiMYSQL57Fixes.php
+++ wiki/install/migrations/20190911131531-WikiMYSQL57Fixes.php
@@ -5,7 +5,7 @@ class WikiMYSQL57Fixes extends CmfiveMigration
     public function up()
     {
         $this->changeColumnInTable("wiki", "last_modified_page_id", "integer", ["null" => true, "default" => null]);
-        $this->changeColumnInTable("wiki_page", "body", "text", ["limit" => 4294967295, "null" => true, "default" => null]);
+        $this->changeColumnInTable("wiki_page", "body", "text", ["limit" => $this->getAdapter()::TEXT_LONG, "null" => true, "default" => null]);
     }
 
     public function down()
